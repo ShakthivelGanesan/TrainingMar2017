6d0f4408328d703832907e44d780c23388409784
saganesa <saganesa@chnsipdt0t617.corp.capgemini.com>
2017-03-29 10:43:29 +0530

    BranchCommit

--- ProductApp_Gradle/src/main/java/org/capgemini/util/ProductUtility.java
+++ ProductApp_Gradle/src/main/java/org/capgemini/util/ProductUtility.java
@@ -5,7 +5,7 @@ public class ProductUtility {
 	private static int productId=0;
 	
 	public static int generateProductId(){
-		System.out.println("Test");
+		System.out.println("Test123");
 		return productId++;
 	}
 
